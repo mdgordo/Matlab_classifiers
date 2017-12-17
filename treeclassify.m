f3e8a3a91b60686cf0c7c35bb360736880bdd585
mdgordo <mdgordo@gmail.com>
2017-12-17 00:35:47 -0500

    Update treeclassify.m

--- treeclassify.m
+++ treeclassify.m
@@ -1,7 +1,11 @@
 function b=treeclassify(y,tr)
 
 for i=1:size(y,1)
-    
-
-
+    node=tr.nodes{1}
+    at=node.attribute
+    if at==0
+        b=root.chat
+    else v=y(i,at)
+        at=node.child{v}
+        node=node.child{v}
 end
