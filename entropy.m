ce216b15ca3e3e217e26aaf662df72813fa014a2
mdgordo <mdgordo@gmail.com>
2017-12-16 23:18:53 -0500

    Update entropy.m

--- entropy.m
+++ entropy.m
@@ -1,3 +1,8 @@
+%% helperfunction for tree train
+%% calculates entropy for a decision tree node
+%% input vector of classes c and number of classes nc
+%% returns entropy e
+
 function e=entropy(c,nc)
     for i=1:nc
         class(i)=sum(c==i);
